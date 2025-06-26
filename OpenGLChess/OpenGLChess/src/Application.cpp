8bd181b784a28b560fa3043d16a8a9797d459a06
Brennan Kersey <bksafari17@gmail.com>
2025-06-26 12:29:05 -0500

    Wrote a Vertex Buffer
    - Learned about vertex buffers for allocation and writing data for rendering
    - Got access to a documentations source that details OpenGL methods

--- OpenGLChess/OpenGLChess/src/Application.cpp
+++ OpenGLChess/OpenGLChess/src/Application.cpp
@@ -1,6 +1,12 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+
+#pragma region Docs
+// Really Good Documentation Website for OpenGL
+//https://docs.gl/
+#pragma endregion
+
 int main(void)
 {
     GLFWwindow* window;
@@ -27,19 +33,37 @@ int main(void)
         std::cout << "Error" << std::endl;
     }
     std::cout << glGetString(GL_VERSION) << std::endl;
+
+
+
+#pragma region ModernGL
+
+    float positions[6] = {
+        -0.5f,-0.5f,
+        0.0f,0.5f,
+        0.5f,-0.5f};
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer); // Creates the buffer and assigns the value to a varaible, this number is used to reference an object as an ID
+    glBindBuffer(GL_ARRAY_BUFFER, buffer); // binds the buffer to a target in this case an array of memory
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float),positions,GL_STATIC_DRAW);
+#pragma endregion
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
+        //glBegin(GL_TRIANGLES);
         
-        glVertex2f(-0.5f,-0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
+        //glVertex2f(-0.5f,-0.5f);
+        //glVertex2f(0.0f, 0.5f);
+        //glVertex2f(0.5f, -0.5f);
         
-        glEnd();
+        //glEnd();
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
