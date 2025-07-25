bf5cbb1c8db0a71153acfc605caa592585e059f3
Brennan Kersey <bksafari17@gmail.com>
2025-07-25 09:25:22 -0500

    MVP Basics
    - Learned that MVP represents the model, view, and projection matrices
    - When these 3 matrices are multiplied together we can get the final vertex position for the rendered frame
    - Added an example in Aplication.cpp

--- OpenGLChess/OpenGLChess/src/Application.cpp
+++ OpenGLChess/OpenGLChess/src/Application.cpp
@@ -95,6 +95,10 @@ int main(void)
 
 #pragma region Projection_Matrix
         glm::mat4 proj = glm::ortho(0.0f, 1280.0f, 0.0f, 720.0f, -1.0f, 1.0f);
+        glm::mat4 view=glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+        glm::mat4 mvp = proj * view* model;
 #pragma endregion
 
 #pragma region Shader
@@ -102,7 +106,7 @@ int main(void)
         Shader so("res/shaders/Basic.shader");
         so.Bind();// We must bind before setting the uniform
         so.SetUniform4f("u_Color",0.8f, 0.3f, 0.8f, 1.0f);
-        so.SetUniformMat4f("u_MVP", proj);
+        so.SetUniformMat4f("u_MVP", mvp);
        
 #pragma endregion Responsible for Passing programs to the GPU for utilizing GPU resources
       
